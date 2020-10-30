193dba71e557b7996d3fbb18004803d6d1226ee1
creattech <crystalsmcneil@gmail.com>
2020-10-30 00:06:59 -0500

    added icons and styling to navbar and footer nav

--- components/footer.js
+++ components/footer.js
@@ -1,32 +1,37 @@
 import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Footer() {
   return (
-    <footer>
-      <nav className="navbar fixed-bottom navbar-expand-sm">
-        <ul className="navbar-nav mr-auto">
-          <li className="nav-item">
-            <Link href="/">
-              <a className="nav-link">Home</a>
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link href="/about">
-              <a className="nav-link">About</a>
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link href="/resources">
-              <a className="nav-link">Resources</a>
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link href="/contact">
-              <a className="nav-link">Contact</a>
-            </Link>
-          </li>
-        </ul>
-      </nav>
+    <footer className="footer mt-auto py-3">
+      <div className="container">
+        <nav className="navbar navbar-expand-sm fixed-bottom">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item mx-4">
+              <Link href="/">
+                <a className="nav-link">
+                  <FontAwesomeIcon icon="house-user" />
+                </a>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link href="/about">
+                <a className="nav-link">About</a>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link href="/resources">
+                <a className="nav-link">Resources</a>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link href="/contact">
+                <a className="nav-link">Contact</a>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </footer>
   );
 }
--- pages/profile.js
+++ pages/profile.js
@@ -61,7 +61,6 @@ const Profile = () => {
               <div className="card mb-4 text-left">
                 <div className="card-body">
                   <h5 className="card-title">
-                    My Name{" "}
                     <span className="float-right">
                       <a>
                         <FontAwesomeIcon icon="user-edit" />
@@ -69,26 +68,25 @@ const Profile = () => {
                     </span>
                   </h5>
                   <img
-                    src="https://bit.ly/3kH35mH"
-                    className="card-img-top"
+                    src="/avatars/avatar_1.png"
+                    className="card-img-top mx-auto d-block"
                     alt="First Name"
                   ></img>
-                  <p className="card-text">
-                    With supporting text below as a natural lead-in to
-                    additional content.
-                  </p>
+                  <small>crystal.mcneil@creativarian.com</small>
+                  <h3>Crystal McNeil</h3>
+                  <p className="card-text">Software Engineer</p>
                 </div>
               </div>
               <div className="card mb-4 text-left">
                 <div className="card-body">
-                  <h5 className="card-title">
+                  <h6 className="card-title">
                     Location{" "}
                     <span className="float-right">
                       <a>
                         <FontAwesomeIcon icon="edit" />
                       </a>
                     </span>
-                  </h5>
+                  </h6>
                   <p className="card-text">
                     With supporting text below as a natural lead-in to
                     additional content.
@@ -100,10 +98,15 @@ const Profile = () => {
               <div className="card mb-4 text-left">
                 <div className="card-body">
                   <h5 className="card-title">Project</h5>
-                  <p className="card-text">
-                    With supporting text below as a natural lead-in to
-                    additional content.
-                  </p>
+                  <h6 className="card-subtitle mb-2 text-muted">
+                    Active Projects
+                  </h6>
+                  <div className="border-top border-dark my-3">
+                    <p className="card-text mt-3">
+                      With supporting text below as a natural lead-in to
+                      additional content.
+                    </p>
+                  </div>
                   <a href="#" className="btn btn-purple-outline btn-block">
                     Go somewhere
                   </a>
@@ -112,10 +115,21 @@ const Profile = () => {
               <div className="card mb-4 text-left">
                 <div className="card-body">
                   <h5 className="card-title">Skills</h5>
-                  <p className="card-text">
-                    With supporting text below as a natural lead-in to
-                    additional content.
-                  </p>
+                  <h6 className="card-subtitle mb-3 text-muted">
+                    Add Skills{" "}
+                    <span className="ml-2">
+                      <FontAwesomeIcon icon="plus" />
+                    </span>
+                  </h6>
+                  <div className="border-top border-dark my-3">
+                    <p className="card-text mt-3">
+                      JavaScript, jQuery, MySQL, Git, Bootstrap RESTful
+                      WebServices, LESS, SASS, Node.js, GraphQL, React, Angular,
+                      XML SQL, MongoDB, REST API, Python, JSON, Django Next.js,
+                      Express, PostgreSQL, PHP, Mongoose, Apollo-Client,
+                      Next.js, Vercel, Heroku
+                    </p>
+                  </div>
                   <a href="#" className="btn btn-purple-outline btn-block">
                     Go somewhere
                   </a>
@@ -125,11 +139,30 @@ const Profile = () => {
             <div className="col">
               <div className="card mb-4 text-left">
                 <div className="card-body">
-                  <h5 className="card-title">My Teams</h5>
+                  <h5 className="card-title">Recommendations</h5>
                   <p className="card-text">
-                    With supporting text below as a natural lead-in to
-                    additional content.
+                    <FontAwesomeIcon className="mr-2" icon="star" />
+                    <FontAwesomeIcon className="mr-2" icon="star" />
+                    <FontAwesomeIcon className="mr-2" icon="star" />
+                    <FontAwesomeIcon className="mr-2" icon="star" />
+                    <FontAwesomeIcon className="mr-2" icon="star" />
                   </p>
+                  <div className="border-top border-dark my-3">
+                    <div className="card-text mt-3">
+                      <blockquote className="blockquote">
+                        <p className="mb-0">
+                          Lorem ipsum dolor sit amet, consectetur adipiscing
+                          elit. Integer posuere erat a ante.
+                        </p>
+                        <footer className="blockquote-footer">
+                          Alan Turing{" "}
+                          <cite title="Source Title">
+                            Sr. Software Engineer
+                          </cite>
+                        </footer>
+                      </blockquote>
+                    </div>
+                  </div>
                   <a href="#" className="btn btn-purple btn-block">
                     Go somewhere
                   </a>
@@ -137,16 +170,21 @@ const Profile = () => {
               </div>
               <div className="card mb-4 text-left">
                 <div className="card-body">
-                  <h5 className="card-title">Recommendations</h5>
-                  <p className="card-text">
-                    With supporting text below as a natural lead-in to
-                    additional content.
-                  </p>
+                  <h5 className="card-title">My Teams</h5>
+                  <h6 className="card-subtitle mb-3 text-muted">
+                    Active Teams
+                  </h6>
+                  <div className="border-top border-dark my-3">
+                    <p className="card-text mt-3">
+                      With supporting text below as a natural lead-in to
+                      additional content.
+                    </p>
+                  </div>
                   <a href="#" className="btn btn-purple btn-block">
                     Go somewhere
                   </a>
                 </div>
-              </div>
+              </div>{" "}
             </div>
           </div>
         </div>
