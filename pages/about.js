21dc7a6f27e9a2b506d7dcd8ea1cfbafcc1876fc
creattech <crystalsmcneil@gmail.com>
2020-11-05 01:34:25 -0600

    updated styling

--- pages/about.js
+++ pages/about.js
@@ -4,7 +4,7 @@ export default function About() {
   return (
     <Layout>
       <div className="container">
-        <div className="row">
+        <div className="row my-4">
           <div className="col">
             <img src="/images/team_task.png" alt="" className="img-fluid" />
           </div>
@@ -14,18 +14,16 @@ export default function About() {
               We're a small team of dedicated developers. We wanted to make it
               simple for people to find team members without a bloated solution.{" "}
             </p>
-            <p>
-              Reasons we love a simple solution.
-              <ul class="list-purple">
-                <li>Don't have to worry about learning a new software.</li>
-                <li>Unlimited projects.</li>
-                <li>Low cost membership.</li>
-                <li>Quickly find members and add them to a project.</li>
-                <li>No big monthly fees.</li>
-                <li>FREE Skill assessment for members.</li>
-                <li>Easily vet team members.</li>
-              </ul>
-            </p>
+            <p>Reasons we love a simple solution.</p>
+            <ul className="list-purple">
+              <li>Don't have to worry about learning a new software.</li>
+              <li>Unlimited projects.</li>
+              <li>Low cost membership.</li>
+              <li>Quickly find members and add them to a project.</li>
+              <li>No big monthly fees.</li>
+              <li>FREE Skill assessment for members.</li>
+              <li>Easily vet team members.</li>
+            </ul>
           </div>
         </div>
       </div>
